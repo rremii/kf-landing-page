33bc3e2f6be32dfa77b587f405bcfd21264f68b8
rremii <noruto2025@ya.ru>
2022-01-02 14:34:13 +0300

    made NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import css from './NavBar.module.sass'
+
+const NavBar = () => {
+    return <div className={css.NavBar}>
+        <div className={css.container}>
+            <span>Monitor</span>
+            <span>Team</span>
+            <span>Testimonials</span>
+        </div>
+        <span className={css.contacts}>Contact</span>
+    </div>
+}
+export default NavBar
