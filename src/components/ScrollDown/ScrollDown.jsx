d6fb874aa5facd78c8650c557985407c075f839b
rremii <noruto2025@ya.ru>
2022-01-03 10:49:45 +0300

    made arrows

--- src/components/ScrollDown/ScrollDown.jsx
+++ src/components/ScrollDown/ScrollDown.jsx
@@ -5,10 +5,11 @@ const ScrollDown = () => {
     return <div className={css.ScrollDown}>
         <span>to login</span>
         <span>Scroll down </span>
-        <div className={css.whiteDiv}/>
-        <div className={css.whiteDiv}/>
-        <div className={css.whiteDiv}/>
-        <div className={css.whiteDiv}/>
+        <div className={css.whiteDiv1}/>
+        <div className={css.whiteDiv2}/>
+        <div className={css.whiteDiv3}/>
+        <div className={css.yellowDiv}/>
+        <div className={css.redDiv}/>
     </div>
 }
 export default ScrollDown
